d3b041397610bc3b46f6d5ae87084c89e335ec17
Billy Nguyen <bquynhnguyen209@gmail.com>
2025-03-24 22:14:12 -0700

    Implemented bonus 2; WIP bonus 1; TODO ask David

--- src/main/java/com/assignment4/EightPuzzle/AStar8PuzzleSolver.java
+++ src/main/java/com/assignment4/EightPuzzle/AStar8PuzzleSolver.java
@@ -18,6 +18,9 @@ public class AStar8PuzzleSolver implements EightPuzzleSolver{
     // Separate chaining hash table or a quadratic probing hash table.
     // TODO - Billy: Explain why?
      private SeparateChainingHashTable<GameBoard> visited;
+
+    // Bonus 2
+    private List<Integer> moves;
     
     public AStar8PuzzleSolver(GameBoard initial, GameBoard goal){
         this.initialBoardState = initial;
@@ -26,12 +29,15 @@ public class AStar8PuzzleSolver implements EightPuzzleSolver{
         predecessors = new HashMap<>();
         solved = solvedStatus.NOT_EXECUTED;
         visited = new SeparateChainingHashTable<>();
+
+        // Bonus 2
+        moves = new LinkedList<>();
     }
 
     public void printSolution(){
         if(solved == solvedStatus.SOLVED){
             for(GameBoard board : reconstructPath(current.board)){
-                //System.out.println(board);
+                System.out.println(board);
             }
         }
     }
@@ -55,35 +61,157 @@ public class AStar8PuzzleSolver implements EightPuzzleSolver{
        * Your code here 
        * Use the exploreNext method to explore the next node in the frontier until the queue is empty
        */
+        // Starts at initial board state
         GameBoardPQEntry start = new GameBoardPQEntry(initialBoardState, 0);
         minPQ.insert(start);
 
+        // Visits the initial board state
         visited.insert(initialBoardState);
 
         while(!minPQ.isEmpty()) {
             exploreNext();
-            GameBoard currentBroadState = current.board;
-            if (currentBroadState.isGoal()) {
+            if (current.board.isGoal() && solutionReached()) {
                 solved = solvedStatus.SOLVED;
-//                Iterable<GameBoard> iterable = reconstructPath(currentBroadState);
+//                Iterable<GameBoard> iterable = reconstructPath(current.board);
+                reconstructPath(current.board);
+//                System.out.println("visited: "+visited);
+                // TODO - Billy: Place where?
+                movesToString();
                 return;
             }
-            //System.out.println("Current board:\n"+currentBroadState.toString());
-            //System.out.println("Hash code: "+currentBroadState.hashCode());
+//            System.out.println("Current board:\n"+current.board.toString());
+//            System.out.println("current.gScore "+current.gScore);
 
+            //System.out.println("Hash code: "+current.board.hashCode());
+
+            // Gets all neighbour board states with pre-set INFINITY gScore value
             Iterable<GameBoardPQEntry> iterable = getNeighbours(current);
+            // Iterate through all the neighbour board states of the current board state
             for (GameBoardPQEntry neighbour : iterable) {
                 //System.out.println("Not Visited: " + !visited.contains(neighbour.board));
                 // TODO - Billy: Verify current.gScore < neighbour.gScore . Is it correct?
-                if (!visited.contains(neighbour.board) || current.gScore < neighbour.gScore) {
-                    //System.out.println("Found neighbour to keep:\n"+neighbour.board.toString());
-                    //System.out.println("Hash code: "+neighbour.board.hashCode());
+                // If neighbour board state has not been visited and the its gScore is larger than current's
+                if (!visited.contains(neighbour.board) && current.gScore < neighbour.gScore) {
+//                    System.out.println("Found neighbour to keep:\n"+neighbour.board.toString());
+//                    System.out.println("Visited: "+visited.contains(neighbour.board));
+//                    System.out.println("neighbour.gScore "+neighbour.gScore);
+
+                    // Visits neighbour board state to prevent looping
                     visited.insert(neighbour.board);
-                    minPQ.insert(neighbour);
+                    predecessors.put(neighbour.board, current.board);
+//                    minPQ.insert(neighbour);
+                    // Insert neighbour board state with current board's gScore incremented by 1 to min heap
+                    minPQ.insert(new GameBoardPQEntry(neighbour.board, current.gScore + 1));
                 }
             }
         }
+        // If all board states are explored, but found no solution, it is impossible to solve
         solved = solvedStatus.NOT_POSSIBLE;
+    }
+
+    /* TODO - Billy (Ask David): Bonus 1
+     * !!! Ask David if "randomly moves a square" has to be
+     *          (1) between 0 and one of its neighbours, or
+     *          (2) between 0 and any random tiles, or
+     *          (3) between any tiles.
+     */
+    public void solveWithRandom() {
+        /*
+         * Your code here
+         * Use the exploreNext method to explore the next node in the frontier until the queue is empty
+         */
+        // Starts at initial board state
+        Random rand = new Random();
+        Boolean hasRandomized = false;
+        GameBoardPQEntry start = new GameBoardPQEntry(initialBoardState, 0);
+        minPQ.insert(start);
+
+        // Visits the initial board state
+        visited.insert(initialBoardState);
+
+        while(!minPQ.isEmpty()) {
+            exploreNext();
+
+            // Decide to move tiles randomly if has never randomized
+            if (rand.nextBoolean() && !hasRandomized)
+            {
+                // Ensure feature can only be done once
+                hasRandomized = true;
+
+                int tile1 = rand.nextInt(0, initialBoardState.dimension())*initialBoardState.dimension();
+                int tile2 = rand.nextInt(1, initialBoardState.dimension())*initialBoardState.dimension();
+
+                // Re-randomize if second tile is the same as first tile
+                while (tile1 == tile2) {
+                    tile2 = rand.nextInt(0, initialBoardState.dimension())*initialBoardState.dimension();
+                }
+
+                System.out.println("Randomly moves tile "+tile1+" to tile "+tile2);
+                current.board.swap(tile1, tile2);
+                // TODO - Billy: Redo heuristic but preserve predecessor
+            }
+
+            if (current.board.isGoal() && solutionReached()) {
+                solved = solvedStatus.SOLVED;
+//                Iterable<GameBoard> iterable = reconstructPath(current.board);
+                reconstructPath(current.board);
+//                System.out.println("visited: "+visited);
+                // TODO - Billy: Place where?
+                movesToString();
+                return;
+            }
+//            System.out.println("Current board:\n"+current.board.toString());
+//            System.out.println("current.gScore "+current.gScore);
+
+            //System.out.println("Hash code: "+current.board.hashCode());
+
+            // Gets all neighbour board states with pre-set INFINITY gScore value
+            Iterable<GameBoardPQEntry> iterable = getNeighbours(current);
+            // Iterate through all the neighbour board states of the current board state
+            for (GameBoardPQEntry neighbour : iterable) {
+                //System.out.println("Not Visited: " + !visited.contains(neighbour.board));
+                // TODO - Billy: Verify current.gScore < neighbour.gScore . Is it correct?
+                // If neighbour board state has not been visited and the its gScore is larger than current's
+                if (!visited.contains(neighbour.board) && current.gScore < neighbour.gScore) {
+//                    System.out.println("Found neighbour to keep:\n"+neighbour.board.toString());
+//                    System.out.println("Visited: "+visited.contains(neighbour.board));
+//                    System.out.println("neighbour.gScore "+neighbour.gScore);
+
+                    // Visits neighbour board state to prevent looping
+                    visited.insert(neighbour.board);
+                    predecessors.put(neighbour.board, current.board);
+//                    minPQ.insert(neighbour);
+                    // Insert neighbour board state with current board's gScore incremented by 1 to min heap
+                    minPQ.insert(new GameBoardPQEntry(neighbour.board, current.gScore + 1));
+                }
+            }
+        }
+        // If all board states are explored, but found no solution, it is impossible to solve
+        solved = solvedStatus.NOT_POSSIBLE;
+    }
+
+    // TODO - Billy (Ask David): Bonus 2 - Is this what he wants per the description?
+    // "Use a suitable data structure to record the order of the moves. Once the algorithm has run you should
+    // be able to call a methods which says ‘move tile 1 to the empty tile, move tile 4 to the empty tile, etc."
+    public void movesToString() {
+        Iterable<GameBoard> iterable = reconstructPath(current.board);
+        GameBoard currentBoard = null;
+        for (GameBoard board : iterable) {
+            // Save and skip first board since it is the initiate board stage
+            if (currentBoard == null) {
+                currentBoard = board;
+                continue;
+            }
+            // Save the tile at the coordinate of the previously empty tile
+            moves.add(board.getTiles()[currentBoard.getEmptyTileRow()][currentBoard.getEmptyTileColumn()]);
+            // Update the current board
+            currentBoard = board;
+        }
+
+        for (int i = 0; i < moves.size() - 1; i++) {
+            System.out.print("move tile " + moves.get(i) + " to the empty tile, ");
+        }
+        System.out.print("move tile " + moves.getLast() + " to the empty tile.\n");
     }
 
     //Explore the next node in the frontier according to the priority queue
